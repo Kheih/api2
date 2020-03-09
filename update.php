d05a646a80b2ea3ddd975d1235bf98154a5d9390
Kheih <56195049+Kheih@users.noreply.github.com>
2020-03-09 19:55:53 +0800

    Update update.php

--- update.php
+++ update.php
@@ -16,7 +16,8 @@
     </head>
 	<br>
 <div id="container">
-			<div id="content">
+	
+		<div id="content">
 					<div class="navbar">
 						<a href="index.php?page=home" id = "home">Home</a>
 						<a href="index.php?page=product" id = "product">Show</a>
@@ -25,26 +26,26 @@
 					</div>
 					
 					
-		<div class="form-style-5">		
+			<div class="form-style-5">		
 				<h1> Update Product </h1>
 				<form action="updateProd.php?id=<?php echo $id; ?>" method="POST">
-					Product:<input type="text" name="name" value="<?php echo $result['name'];?>"/>
+					Product:<input type="text" name="name" value="<?php echo $result['name']; ?>"/>
 					Description:<input type="text" name="description" value="<?php echo $result['description']; ?>"/>
 					Price:<input type="text" name="price" value="<?php echo $result['price']; ?>"/>
 					Category:<select name="category">
-					<option value="<?php echo $result['category_id'];?>"><?php echo $result['category_name'];?></option>
+							<option value="<?php echo $result['category_id'];?>"><?php echo $result['category_name'];?></option>
 						<?php
-						foreach($category as $cview){
+								foreach($category as $cview){
 						?>
 							<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
 						<?php
 						}
 						?>
 						</select>
-					<input type="submit" name="submit" value="submit"/>
+					<input type="submit" name="submit" value="Update"/>
 
 				</form>
-		</div>	
 			</div>	
-</div>	
+		</div>	
+	</div>	
 </html>
