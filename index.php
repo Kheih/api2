844eea465d3308d317ee2344ba843765681d35b9
Kheih <56195049+Kheih@users.noreply.github.com>
2020-03-09 18:32:49 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page']: '';
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+      <title>Endterm Activity #1</title>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+
+  <div id="container">  
+      <div class="navbar">
+        <a href="index.php?page=product" id = "product">Products</a>
+        <a href="index.php?page=categories" id = "category">Category</a>
+        <a href="index.php?page=create" id = "create">Add Product</a>
+      </div>
+
+    <div id="content">         
+      <?php 
+        switch($page){
+          case 'readone':
+			 require_once('readOne.php');
+				        break;
+				case 'categories':
+			      require_once('categories.php');
+			          break;
+        case 'create':
+        		require_once('form_create.php');
+        			  break;
+        case 'delete':
+        		require_once('delete.php');
+        			  break;
+        case 'update':
+            require_once('update.php');
+                break;
+				default:
+					  require_once('read_all.php');
+					      break;
+        }
+    ?>
+      </div>
+  </div>    
+    </body>
+</html>
