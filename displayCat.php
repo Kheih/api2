baead78d07fd8201ceef5ef91f98303ea27b9dc9
Kheih <56195049+Kheih@users.noreply.github.com>
2020-03-09 18:50:10 +0800

    Create displayCat.php

---
+++ displayCat.php
@@ -0,0 +1,28 @@
+<div>
+  <?php
+    $json = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
+    $data = json_decode($json,true);
+
+    $list = $data['records'];
+  ?>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <h1>Categories</h1>
+  
+ <table id="list">
+      <tr>
+       <th>ID</th>
+       <th>Name</th>
+       <th>Description</th>
+      </tr>
+    <?php
+     foreach($list as $result){
+    ?>
+    <tr>
+      <td> <?php echo $result['id']; ?></a> </td>
+      <td> <?php echo $result['name']; ?></a> </td>
+      <td> <?php echo $result['description']; ?></a> </td>
+    </tr>
+  <?php
+   }
+  ?>
+</div>
